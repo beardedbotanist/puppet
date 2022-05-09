b73bb242324725d4f9828ecc983f3f0b6a858452
beardedbotanist <patsully340@icloud.com>
2022-05-09 13:07:10 -0700

    changing command line

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/production && git pull
-/usr/local/bin/puppet apply manifests/
+/usr/local/bin/puppet apply --modulepath /etc/puppetlabs/code/environments/production/modules/ --verbose manifests
