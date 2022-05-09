2c0ac827fc97128301f44b1a2facb78a42485a14
beardedbotanist <patsully340@icloud.com>
2022-05-09 12:57:46 -0700

    changing schedule back to cron

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,14 +1,6 @@
 # Set up regular Puppet runs
-file { '/usr/local/bin/run-puppet':
-  source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
-  mode   => '0755',
-}
-
-schedule { 'every15min':
-  period => hourly,
-  repeat => 4,
-}
-
-exec { "/usr/local/bin/run-puppet": 
-  schedule => 'every15min'
+   cron { 'run-puppet':
+     command => '/usr/local/bin/run-puppet',
+     hour    => '*',
+     minute  => '*/15',
 }
